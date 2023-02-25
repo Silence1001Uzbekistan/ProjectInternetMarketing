09f34ea2b323630dfab60dc3f18303d1a1fb598f
Doniyor Artikov <doniyorartikov1999@gmail.com>
2023-02-25 05:37:10 -0800

    25.02.23,18:36

--- app/src/main/java/com/example/internetmarketing/Papka/QuizResults.kt
+++ app/src/main/java/com/example/internetmarketing/Papka/QuizResults.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.widget.AppCompatButton
 import android.widget.TextView
 import android.content.Intent
+import android.graphics.Color
 import android.view.WindowManager
 import com.example.internetmarketing.HomeActivity
 import com.example.internetmarketing.R
@@ -23,10 +24,28 @@ class QuizResults : AppCompatActivity() {
         )
 
         val startNewBtn = findViewById<AppCompatButton>(R.id.startNewQuizBtn)
+        val processAnswer = findViewById<TextView>(R.id.processAnswers)
         val correctAnswer = findViewById<TextView>(R.id.correctAnswers)
         val incorrectAnswer = findViewById<TextView>(R.id.incorrectAnswers)
         val getCorrectAnswers = intent.getIntExtra("correct", 0)
         val getInCorrectAnswers = intent.getIntExtra("incorrect", 0)
+
+        if (getCorrectAnswers <= 5){
+            processAnswer.text = "Yomon"
+            processAnswer.setTextColor(Color.RED)
+        }else{
+            if (getCorrectAnswers in 6..9){
+                processAnswer.text = "Yaxshi"
+                processAnswer.setTextColor(Color.MAGENTA)
+            }else{
+
+                if (getCorrectAnswers in 11..20){
+                    processAnswer.text = "A'lo"
+                    processAnswer.setTextColor(Color.YELLOW)
+                }
+
+            }
+        }
         correctAnswer.text = getCorrectAnswers.toString()
         incorrectAnswer.text = getInCorrectAnswers.toString()
         startNewBtn.setOnClickListener {
